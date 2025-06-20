602980e9bd33d20842878a1b03d60584068f3017
reyza121203 <reizaerviananda121203@gmail.com>
2025-06-20 09:04:12 +0700

    Add files via upload

---
+++ Player.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.gamemultiplayer;
+
+public class Player {
+    int x = 100, y = 100;
+    int score = 0;
+
+    public void setPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
